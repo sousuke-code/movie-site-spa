6fbc5af5752b56695f4e4526a6d52e7d13ab4a27
sousuke-code <162286022+sousuke-code@users.noreply.github.com>
2024-09-27 23:23:54 +0900

    ローディング画面

--- resources/js/pages/HomePage.jsx
+++ resources/js/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import axios from 'axios';
+import { Audio } from 'react-loader-spinner';
 
 const HomePage = () => {
   const [movies,setMovies ] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // const data = localStorage.getItem('auth_id');
   useEffect(()=> {
@@ -17,26 +19,46 @@ const HomePage = () => {
         if (res.data.status === 200 ) {
           setMovies(res.data.movies);
         }
+        setLoading(false);
       })
     })
   },[]);
 
-  return (
-    <>
-    {movies.length > 0 ? (
-      (movies.map((movie) => {
-        return (
-          <div key={movie.id} className='relative m-4'>
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" className='rounded h-full'/>
-          </div>
-        )
-      })))
-    : (
-      <p className='text-black font-semibold flex justify-center text-lg'>映画が見つかりませんでした</p>
+  if (loading === true) {
+    return (
+      <div className='flex justify-center items-center h-screen'>
+        <Audio
+          height="150"
+          width="150"
+          color="black"
+          ariaLabel="audio-loading"
+          wrapperStyle={{}}
+          wrapperClass="wrapper-class"
+          visible={true}
+          />
+    </div>
     )
+    
+  }else {
+    return (
+      <div className='grid grid-cols-3'>
+      {movies.length > 0 ? (
+        (movies.map((movie) => {
+          return (
+            <div key={movie.id} className='relative m-4'>
+              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" className='rounded h-full'/>
+            </div>
+          )
+        })))
+      : (
+        <p className='text-black font-semibold flex justify-center text-lg'>映画が見つかりませんでした</p>
+      )
+    }
+     </div>
+    )
+
   }
-   </>
-  )
+
 }
 
 export default HomePage
