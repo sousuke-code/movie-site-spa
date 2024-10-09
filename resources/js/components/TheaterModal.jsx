dde11541a2d5dab632136444f0523a001610840a
sousuke-code <162286022+sousuke-code@users.noreply.github.com>
2024-10-09 18:08:23 +0900

    施設までの所要時間の計算

--- resources/js/components/TheaterModal.jsx
+++ resources/js/components/TheaterModal.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IoMdClose } from "react-icons/io";
+import axios from "axios";
 
-const TheaterModal = ({ modal, setModal, theater }) => {
+const TheaterModal = ({ modal, setModal, theater,lat,lng }) => {
     const closeModal = () => {
         setModal(false);
     };
-
+		
 		const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+
+		const calculateDistance = async() => {
+			const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${lat},${lng}&destinations=${theater.geometry.location.lat},${theater.geometry.location.lng}&key=${apiKey}`;
+
+			try {
+				const response = await axios.get(url);
+				const result = response.data;
+				console.log(result);
+			} catch (error) {
+				console.log('error, frtch');
+			}
+		}
+
 		
 		console.log(apiKey)
+
+		useEffect(() => {
+			if (modal) {
+				calculateDistance();
+			}
+		},[modal]);
     if (modal) {
         return (
             <div id="overlay">
@@ -21,15 +41,7 @@ const TheaterModal = ({ modal, setModal, theater }) => {
 									</div>
 
 									<div className="">
-									{
-										theater.photos &&  (
-											<img
-													src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&maxheight=300&photoreference=${theater.photos[0].photo_reference}&key=${apiKey}`}
-													alt={theater.name}
-													className="w-full"
-											/>
-									)
-									}
+									
 									<div className="py-2 gap-2">
                     <h1 className="font-bold flex items-center justify-center text-xl">{theater.name}											
 										</h1>
--- resources/js/pages/TheaterView.jsx
+++ resources/js/pages/TheaterView.jsx
@@ -1,73 +1,74 @@
 import React from "react";
-import { GoogleMap, LoadScript,Marker } from "@react-google-maps/api";
-import { useEffect,useState} from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
 import { TbCurrentLocation } from "react-icons/tb";
 import axios from "axios";
 import TheaterModal from "../components/TheaterModal";
 
 const TheaterView = () => {
-	const [theaters, setTheaters] = useState([]);
-	const [modal, setModal] = useState(false);
-	const [selectTheater, setSelectTheater] = useState([]);
-	
+    const [theaters, setTheaters] = useState([]);
+    const [modal, setModal] = useState(false);
+    const [selectTheater, setSelectTheater] = useState([]);
 
+    //現在地の初期化
+    const lat = 0;
+    const lng = 0;
 
-const defaultCenter = {
-		lat: 35.66581861, lng: 139.72951166,
-}
+    const fetchTheaters = async (lat, lng) => {
+        const response = await axios.get("/api/theaters", {
+            params: { lat, lng },
+        }); // Laravelのエンドポイントを呼び出す
+        setTheaters(response.data.results);
+        console.log(response.data);
+    };
 
-	const fetchTheaters = async (lat, lng) => {
-			const response = await axios.get('/api/theaters', {
-				params: {lat, lng},
-			}); // Laravelのエンドポイントを呼び出す
-			setTheaters(response.data.results);
-			console.log(response.data);
-	};
+    const SearchGeolocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+            fetchTheaters(lat, lng);
+        });
+    };
 
+    const openModal = (theater) => {
+        setModal(true);
+        setSelectTheater(theater);
+    };
 
+    console.log(theaters);
 
+    return (
+        <>
+            <div className="m-10 lg:m-20 bg-white shadow rounded">
+                <div className="flex items-center justify-center text-xl font-bold mt-10 p-2">
+                    <button type="button" onClick={SearchGeolocation}>
+                        現在地から探す
+                    </button>
+                    <TbCurrentLocation className="text-2xl" />
+                </div>
 
-
-const SearchGeolocation = () => {
-   navigator.geolocation.getCurrentPosition(position => {
-		const lat = position.coords.latitude;
-		const lng = position.coords.longitude;
-		fetchTheaters(lat,lng);
-
-})}
-
-const openModal = (theater) => {
-   setModal(true);
-	 setSelectTheater(theater);
-}
-
-
-
-console.log(theaters);
-
-return (
-	<>
-
-  <div className="m-10 lg:m-20 bg-white shadow rounded">
-	 <div className="flex items-center justify-center text-xl font-bold mt-10 p-2">
-	 <button type="button" onClick={SearchGeolocation} >現在地から探す</button>
-	 <TbCurrentLocation className="text-2xl"/>
-		</div>
-           
-  <ul className=" divide-gray-100 shadow bg-white rounded">
-						{theaters.map((theater,index) => (
-							<li className="flex justify between py-5 border-bottom"  onClick={() => openModal(theater)}>
-								<div className="flex m-2">
-								<h1>{theater.name}</h1>
-								</div>
-							</li >
-						))}
-
-	</ul>
-	<TheaterModal modal={modal} setModal={setModal} theater={selectTheater}/>
-	</div>
-	</>
- )
+                <ul className=" divide-gray-100 shadow bg-white rounded">
+                    {theaters.map((theater, index) => (
+                        <li
+                            className="flex justify between py-5 border-bottom"
+                            onClick={() => openModal(theater)}
+                        >
+                            <div className="flex m-2">
+                                <h1>{theater.name}</h1>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <TheaterModal
+                    modal={modal}
+                    setModal={setModal}
+                    theater={selectTheater}
+                    lat={lat}
+                    lng={lng}
+                />
+            </div>
+        </>
+    );
 };
 
 export default TheaterView;
